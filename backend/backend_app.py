8bf7d9203ed416b991af6e6f27afbf263be175ab
Sethugha <gerd.furmanek@gmx.net>
2025-05-10 01:06:32 +0200

    created CRUP features

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,18 +1,105 @@
-from flask import Flask, jsonify
+import logging
+from flask import Flask, jsonify, request
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 from flask_cors import CORS
+import storage
 
 app = Flask(__name__)
+limiter = Limiter(app=app, key_func=get_remote_address)
 CORS(app)  # This will enable CORS for all routes
 
-POSTS = [
-    {"id": 1, "title": "First post", "content": "This is the first post."},
-    {"id": 2, "title": "Second post", "content": "This is the second post."},
-]
-
+#configure logging
+#logging.basicConfig(filename='log.txt', filemode='w', level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
 @app.route('/api/posts', methods=['GET'])
+#@limiter.limit("10/minute") #Limit to 10 requests per minute
 def get_posts():
-    return jsonify(POSTS)
+    posts = storage.load_json("data.json")
+    title = request.args.get('title')
+
+    if title:
+        collection = [post for post in posts if post.get('title') == title]
+        return jsonify(collection)
+    """
+    page = int(request.args.get('page', 1))
+    limit = int(request.args.get('limit', 10))
+    if page and limit:
+        start_index = (page - 1) * limit
+        end_index = start_index + limit
+        paginated_posts = posts[start_index:end_index]
+        return jsonify(paginated_posts)
+    """
+    return jsonify(posts)
+
+
+@app.route('/api/posts', methods=['GET', 'POST'])
+def posts():
+    if request.method == 'GET':
+        # Handle the GET request
+        posts = storage.load_json("data.json")
+        return jsonify(posts)
+
+    elif request.method == 'POST':
+        # Handle the POST request
+        new_post = request.get_json()
+        if not validate_post_data(new_post):
+            return jsonify({"error": "Invalid post data"}), 400
+        storage.add_post(new_post)
+        return jsonify(new_post), 201
+
+
+@app.route('/api/posts/<int:id>', methods=['PUT'])
+def handle_post(id):
+    # Find the post with the given ID
+    post = storage.find_post_by_id(id)
+
+    # If the post wasn't found, return a 404 error
+    if post is None:
+        return '', 404
+
+    # Update the post with the new data
+    new_data = request.get_json()
+    updated_post = storage.update_post(post, new_data)
+    # Return the updated post
+    return jsonify(updated_post)
+
+
+@app.route('/api/posts/<int:id>', methods=['DELETE'])
+def delete_post(id):
+    # Find the post with the given ID
+    post = storage.find_post_by_id(id)
+
+    # If the post wasn't found, return a 404 error
+    if post is None:
+        return '', 404
+
+    # Remove the book from the list
+    deleted_post = storage.delete_post(id)
+
+    # Return the deleted post
+    return jsonify(deleted_post)
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return jsonify({"error": "Post Not Found or Wrong Url"}), 404
+
+
+@app.errorhandler(405)
+def method_not_allowed_error(error):
+    return jsonify({"error": "Method Not Allowed"}), 405
+
+
+@app.errorhandler(400)
+def bad_request(error):
+    return jsonify({"error": "Request is in bad shape. Missing some field data?"}), 400
+
+
+def validate_post_data(data):
+    if "title" not in data or "content" not in data:
+        return False
+    return True
 
 
 if __name__ == '__main__':
---
+++ backend/storage.py
@@ -0,0 +1,69 @@
+import json
+
+
+def save_json(posts):
+    with open('data.json', 'w', encoding='utf-8') as file:
+        json.dump(posts, file, ensure_ascii=False, indent=4)
+
+
+def load_json(filepath):
+    with open(filepath, 'r') as file:
+        posts = json.load(file)
+    return posts
+
+
+def get_highest_id():
+    ids = []
+    posts = load_json('data.json')
+    for post in posts:
+        ids.append(post['id'])
+    return max(ids)
+
+
+def delete_post(id):
+    posts = load_json('data.json')
+    for post in posts:
+        if post['id'] == id:
+            posts.remove(post)
+            save_json(posts)
+            return post
+    return None
+
+
+def add_post(post):
+    posts = load_json('data.json')
+    new_id = get_highest_id() + 1
+    post['id'] = new_id
+    posts.append(post)
+    save_json(posts)
+
+
+def find_post_by_id(id):
+    posts = load_json('data.json')
+    for post in posts:
+        if posts['id'] == id:
+            return post
+    return None
+
+
+def update_post(updated_post, changes):
+    posts = load_json('data.json')
+    for post in posts:
+        if post['id'] == updated_post['id']:
+            post.update(changes)
+            save_json(posts)
+            return post
+    return None
+
+
+def main():
+    posts = [
+        {"id": 1, "title": "First post", "content": "This is the first post."},
+        {"id": 2, "title": "Second post", "content": "This is the second post."},
+    ]
+    save_json(posts)
+    get_highest_id()
+
+
+if __name__ == '__main__':
+    main()
